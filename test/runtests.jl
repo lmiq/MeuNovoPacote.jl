e2aafa81a9186e889b460173041385a1bbc7d87e
Leandro Martinez <lmartine@unicamp.br>
2025-01-31 09:32:26 -0300

    add alloc test that fails

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,10 @@
 using TestItemRunner
 @run_package_tests
+
+@testitem "PDBTools failed test" begin
+    using PDBTools
+    using BenchmarkTools
+    atoms = read_pdb(PDBTools.TESTPDB, "protein")
+    a = @benchmark sum($mass, $atoms) samples=1 evals=1
+    @test a.allocs == 0
+end
